9db6ccc8fb09e037dd05ebf050497d124cdf4957
Nidhi Kumari <nidhi1691996@outlook.com>
2020-02-13 23:26:28 +0530

    Registration Form added

--- src/containers/RegistrationForm/components/FormPersonalDetails.js
+++ src/containers/RegistrationForm/components/FormPersonalDetails.js
@@ -1,76 +1,353 @@
-import React, { Component } from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import AppBar from '@material-ui/core/AppBar';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import "./style.css";
 
-export class FormPersonalDetails extends Component {
-  continue = e => {
-    e.preventDefault();
-    this.props.nextStep();
-  };
+class FormPersonalDetails extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fields: {},
+      errors: {}
+    };
 
-  back = e => {
+    this.handleChange = this.handleChange.bind(this);
+    this.submituserRegistrationForm = this.submituserRegistrationForm.bind(
+      this
+    );
+  }
+
+  handleChange(e) {
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
+    this.setState({
+      fields
+    });
+  }
+
+  submituserRegistrationForm(e) {
     e.preventDefault();
-    this.props.prevStep();
-  };
+    if (this.validateForm()) {
+      let fields = {};
+      fields["participantcode"] = "";
+      fields["participantname"] = "";
+      fields["contactperson"] = "";
+      fields["emailid"] = "";
+      fields["mobileno"] = "";
+      fields["address"] = "";
+      fields["bankname"] = "";
+      fields["bankbranch"] = "";
+      fields["bankifsc"] = "";
+      fields["bankaccountno"] = "";
+      fields["pancard"] = "";
+      fields["officeadress"] = "";
+
+      this.setState({ fields: fields });
+      alert("Form submitted");
+    }
+  }
+
+  validateForm() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    if (!fields["participantcode"]) {
+      formIsValid = false;
+      errors["participantcode"] = "*Please enter your participantcode.";
+    }
+
+    if (typeof fields["participantcode"] !== "undefined") {
+      if (!fields["participantcode"].match(/^[0-9]{10}$/)) {
+        formIsValid = false;
+        errors["participantcode"] = "*Please enter valid participantcode";
+      }
+    }
+
+    if (!fields["participantname"]) {
+      formIsValid = false;
+      errors["participantname"] = "*Please enter your participantname.";
+    }
+
+    if (typeof fields["participantname"] !== "undefined") {
+      if (!fields["participantname"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["participantname"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["contactperson"]) {
+      formIsValid = false;
+      errors["contactperson"] = "*Please enter your contactperson.";
+    }
+
+    if (typeof fields["contactperson"] !== "undefined") {
+      if (!fields["contactperson"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["contactperson"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["emailid"]) {
+      formIsValid = false;
+      errors["emailid"] = "*Please enter your email-ID.";
+    }
+
+    if (typeof fields["emailid"] !== "undefined") {
+      //regular expression for email validation
+      var pattern = new RegExp(
+        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+      );
+      if (!pattern.test(fields["emailid"])) {
+        formIsValid = false;
+        errors["emailid"] = "*Please enter valid email-ID.";
+      }
+    }
+
+    if (!fields["mobileno"]) {
+      formIsValid = false;
+      errors["mobileno"] = "*Please enter your mobile no.";
+    }
+
+    if (typeof fields["mobileno"] !== "undefined") {
+      if (!fields["mobileno"].match(/^[0-9]{10}$/)) {
+        formIsValid = false;
+        errors["mobileno"] = "*Please enter valid mobile no.";
+      }
+    }
+
+    if (!fields["address"]) {
+      formIsValid = false;
+      errors["address"] = "*Please enter your address.";
+    }
+
+    if (typeof fields["address"] !== "undefined") {
+      if (!fields["address"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["address"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["bankname"]) {
+      formIsValid = false;
+      errors["bankname"] = "*Please enter your bankname";
+    }
+
+    if (typeof fields["bankname"] !== "undefined") {
+      if (!fields["bankname"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["bankname"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["bankifsc"]) {
+      formIsValid = false;
+      errors["bankifsc"] = "*Please enter your bankifsc";
+    }
+
+    if (typeof fields["bankifsc"] !== "undefined") {
+      if (!fields["bankifsc"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["bankifsc"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["bankaccountno"]) {
+      formIsValid = false;
+      errors["bankaccountno"] = "*Please enter your bankaccountno";
+    }
+
+    if (typeof fields["bankaccountno"] !== "undefined") {
+      if (!fields["bankaccountno"].match(/^[0-9]{10}$/)) {
+        formIsValid = false;
+        errors["bankaccountno"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["pancard"]) {
+      formIsValid = false;
+      errors["pancard"] = "*Please enter your email-ID.";
+    }
+
+    if (typeof fields["pancard"] !== "undefined") {
+      //regular expression for email validation
+      var pattern = new RegExp(/^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/);
+      if (!pattern.test(fields["pancard"])) {
+        formIsValid = false;
+        errors["pancard"] = "*Please enter valid pancard";
+      }
+    }
+
+    if (!fields["officeadress"]) {
+      formIsValid = false;
+      errors["officeadress"] = "*officeadress";
+    }
+
+    if (typeof fields["officeadress"] !== "undefined") {
+      //regular expression for email validation
+      var pattern = new RegExp(/^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/);
+      if (!pattern.test(fields["officeadress"])) {
+        formIsValid = false;
+        errors["officeadress"] = "*Please enter valid officeadress.";
+      }
+    }
+
+    // if (!fields["password"]) {
+    //   formIsValid = false;
+    //   errors["password"] = "*Please enter your password.";
+    // }
+
+    // if (typeof fields["password"] !== "undefined") {
+    //   if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+    //     formIsValid = false;
+    //     errors["password"] = "*Please enter secure and strong password.";
+    //   }
+    // }
+
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
+  }
 
   render() {
-    const { values, handleChange } = this.props;
     return (
-      <MuiThemeProvider >
-        <React.Fragment>
-        <Dialog 
-            open="true"
-            fullWidth="true"
-            maxWidth='sm'
+      <div id="main-registration-container">
+        <div id="register">
+          <h3>Registration page</h3>
+          <form
+            method="post"
+            name="userRegistrationForm"
+            onSubmit={this.submituserRegistrationForm}
           >
-            <AppBar title="Enter Personal Details" />
-            <TextField
-              placeholder="Enter Your Occupation"
-              label="Occupation"
-              onChange={handleChange('occupation')}
-              defaultValue={values.occupation}
-              margin="normal"
-              fullWidth="true"
-            />
-            <br />
-            <TextField
-              placeholder="Enter Your City"
-              label="City"
-              onChange={handleChange('city')}
-              defaultValue={values.city}
-              margin="normal"
-							fullWidth="true"
-            />
-            <br />
-            <TextField
-              placeholder="Enter Your Bio"
-              label="Bio"
-              onChange={handleChange('bio')}
-              defaultValue={values.bio}
-              margin="normal"
-							fullWidth="true"
-            />
-            <br />
-        
-            <Button
-              color="secondary"
-              variant="contained"
-              onClick={this.back}
-            >Back</Button>
 
-            <Button
-              color="primary"
-              variant="contained"
-              onClick={this.continue}
-            >Continue</Button>
-          </Dialog>
-        </React.Fragment>
-      </MuiThemeProvider>
+
+          <label>Participant code</label>
+            <input
+              type="text"
+              name="participantcode"
+              value={this.state.fields.participantcode}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.username}</div>{" "}
+
+            <label>Participant Name</label>
+            <input
+              type="text"
+              name="participantname"
+              value={this.state.fields.username}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.participantname}</div>{" "}
+
+            <label>Contact Person</label>
+            <input
+              type="text"
+              name="contactperson"
+              value={this.state.fields.contactperson}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.contactperson}</div>{" "}
+
+            <label>Email ID:</label>
+            <input
+              type="text"
+              name="emailid"
+              value={this.state.fields.emailid}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.emailid}</div>
+
+            <label>Mobile No:</label>
+            <input
+              type="text"
+              name="mobileno"
+              value={this.state.fields.mobileno}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.mobileno}</div>
+           
+            <label>Address</label>
+            <input
+              type="text"
+              name="address"
+              value={this.state.fields.address}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.address}</div>{" "}
+
+            <label>Bank Name</label>
+            <input
+              type="text"
+              name="bankname"
+              value={this.state.fields.bankname}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.bankname}</div>{" "}
+
+            <label>Bank branch</label>
+            <input
+              type="text"
+              name="bankbranch"
+              value={this.state.fields.bankbranch}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.bankbranch}</div>{" "}
+
+            <label>Bank IFSC</label>
+            <input
+              type="text"
+              name="bankifsc"
+              value={this.state.fields.bankifsc}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.bankifsc}</div>
+           
+            <label>Bank Account No.</label>
+            <input
+              type="text"
+              name="bankaccountno"
+              value={this.state.fields.bankaccountno}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.bankaccountno}</div>
+            
+
+            <label>PAN No.</label>
+            <input
+              type="text"
+              name="pancard"
+              value={this.state.fields.pancard}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.pancard}</div>
+
+
+
+            {/* <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              value={this.state.fields.password}
+              onChange={this.handleChange}
+            /> */}
+
+            <label>Office Address</label>
+            <input
+              type="text"
+              name="officeaddress"
+              value={this.state.fields.officeaddress}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.officeaddress}</div>{" "}
+{/* 
+            <div className="errorMsg">{this.state.errors.password}</div> */}
+
+
+            <input type="submit" className="button" value="Register" />
+
+          </form>
+        </div>
+      </div>
     );
   }
 }
-
 export default FormPersonalDetails;
