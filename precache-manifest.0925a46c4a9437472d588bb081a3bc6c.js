0766d48f08906c00e95076190d8cf5a57ca08f49
Nidhi Kumari <nidhi1691996@outlook.com>
2020-02-21 14:56:20 +0530

    Updates

--- precache-manifest.0925a46c4a9437472d588bb081a3bc6c.js
+++ precache-manifest.0925a46c4a9437472d588bb081a3bc6c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08cf5f2999dd2ee277ab",
+    "revision": "a6705238c9cddbcd9d3f",
     "url": "/dash-mash/static/css/main.5ffd6d79.chunk.css"
   },
   {
-    "revision": "08cf5f2999dd2ee277ab",
-    "url": "/dash-mash/static/js/main.08cf5f29.chunk.js"
+    "revision": "a6705238c9cddbcd9d3f",
+    "url": "/dash-mash/static/js/main.a6705238.chunk.js"
   },
   {
     "revision": "d90738ea5d2d01ae24a2",
     "url": "/dash-mash/static/js/runtime~main.d90738ea.js"
   },
   {
-    "revision": "836320eede118bcee80b",
+    "revision": "0828534088091ce8be8a",
     "url": "/dash-mash/static/css/2.c6250e3a.chunk.css"
   },
   {
-    "revision": "836320eede118bcee80b",
-    "url": "/dash-mash/static/js/2.836320ee.chunk.js"
+    "revision": "0828534088091ce8be8a",
+    "url": "/dash-mash/static/js/2.08285340.chunk.js"
   },
   {
     "revision": "63ee4475466140cb853e7cedae8026c0",
@@ -29,10 +29,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "685ccfcff289e14b4ad477eb2bd78307",
-    "url": "/dash-mash/static/js/2.836320ee.chunk.js.LICENSE.txt"
+    "url": "/dash-mash/static/js/2.08285340.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b8e68dedf3246f7e8dc1635ab9f3055e",
+    "revision": "b9c96e06199151bf4b47e92078e466f7",
     "url": "/dash-mash/index.html"
   }
 ]);
